b2396c10b712ea92d90988a0edd889cd5c2885bf
DabliuArte <dabliuarte@gmail.com>
2016-09-13 21:14:10 -0300

    Sessão

---
+++ Sessao/src/java/Pedido.java
@@ -0,0 +1,44 @@
+
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RA21507613
+ */
+public class Pedido {
+    private String molho;
+    private String massa;
+    private List<String> ingedientes;
+
+    public String getMolho() {
+        return molho;
+    }
+
+    public void setMolho(String molho) {
+        this.molho = molho;
+    }
+
+    public String getMassa() {
+        return massa;
+    }
+
+    public void setMassa(String massa) {
+        this.massa = massa;
+    }
+
+    public List<String> getIngedientes() {
+        return ingedientes;
+    }
+
+    public void setIngedientes(List<String> ingedientes) {
+        this.ingedientes = ingedientes;
+    }
+    
+                                                                                                                                                                                                                                  
+}
